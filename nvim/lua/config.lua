972a72a6b9317f42c49ad21afa0892e8c37d1e15
Tommy Karlsson <tommy.karlsson@fortnox.se>
2024-03-08 09:30:13 +0100

    Update to new pathswq

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -157,7 +157,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-	vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+	vim.keymap.set('i', '<C-A>', 'copilot#Accept("\\<CR>")', {
 		expr = true,
 		replace_keycodes = false
 	})
